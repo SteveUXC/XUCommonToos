813c3468fe14ec957fe2aca847975586d25f16de
SteveJameoson <xuanyuan0511@gmail.com>
2017-03-21 13:46:16 +0800

    添加了image圆角扩展

---
+++ XUCOmmonToos/XUCOmmonToos/Tools/UIImage+XUExtension.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+XUExtension.h
+//  MyZML
+//
+//  Created by SteveJameson on 2017/2/22.
+//  Copyright © 2017年 SteveJameson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (XUExtension)
+
+
+// 获得一个image的圆角样式
+- (UIImage *)circlrImage;
+
+@end
