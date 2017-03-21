680501217938b739b10bec8a2a8d0a168e596df8
SteveJameoson <xuanyuan0511@gmail.com>
2017-03-21 10:37:50 +0800

    初始化项目

---
+++ XUCOmmonToos/XUCOmmonToos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XUCOmmonToos
+//
+//  Created by CNCOMMDATA on 2017/3/21.
+//  Copyright © 2017年 CNCOMMDATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
